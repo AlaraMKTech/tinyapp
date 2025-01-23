b8d02f959378ab674b4c57cf199744d3275c38d5
Heidi Dodge <heidi.maria.93@hotmail.com>
2025-01-23 17:50:57 -0330

    Modified to add cookie-parser

--- express_server.js
+++ express_server.js
@@ -30,21 +30,41 @@ app.get("/urls.json", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = {
+    username: req.cookies["username"],
+    urls: urlDatabase,
+  };
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = {
+    username: req.cookies["username"],
+  };
+  res.render("urls_new", templateVars);
 });
 
 app.get("/urls/:id", (req, res) => {
   const shortURL = req.params.id;
   const longURL = urlDatabase[shortURL];
-  const templateVars = { id: shortURL, longURL: longURL };
+  const templateVars = {
+    id: shortURL,
+    longURL: longURL,
+    username: req.cookies["username"],
+  };
   res.render("urls_show", templateVars);
 });
 
+app.post("/login", (req, res) => {
+  const username = req.body.username;
+  res.cookie("username", username);
+  res.redirect("/urls");
+});
+
+app.get("/logout", (req, res) => {
+  res.clearCookie("username");
+  res.redirect("/urls");
+});
 
 app.post("/urls/:id", (req, res) => {
   const shortURL = req.params.id;
