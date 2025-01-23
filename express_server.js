e73c3d92e72866299194057c451659c036e2d99b
Heidi Dodge <heidi.maria.93@hotmail.com>
2025-01-23 18:03:09 -0330

    Implemented client logout

--- express_server.js
+++ express_server.js
@@ -59,9 +59,9 @@ app.post("/login", (req, res) => {
   const username = req.body.username;
   res.cookie("username", username);
   res.redirect("/urls");
-});
+})
 
-app.get("/logout", (req, res) => {
+app.post("/logout", (req, res) => {
   res.clearCookie("username");
   res.redirect("/urls");
 });
