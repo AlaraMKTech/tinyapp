720022c77e7cb1d29554b43452e208c2d0267b6e
Heidi Dodge <heidi.maria.93@hotmail.com>
2025-01-26 16:00:28 -0330

    Refactoring login

--- express_server.js
+++ express_server.js
@@ -82,7 +82,7 @@ app.post("/login", (req, res) => {
 
 app.post("/logout", (req, res) => {
   res.clearCookie("user_id");
-  res.redirect("/urls");
+  res.redirect("/login");
 });
 
 app.get("/", (req, res) => {
